55130f4e7899a6a36094ae7f4566caab72b18a8b
Dennis-K-Koros <denniskilpy@gmail.com>
2024-05-22 17:19:04 +0300

    Made some changes after kuelewa

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ require('./config/db');
 const app = require('express')();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
-
 const UserRouter = require('./api/user');
 
 //for accepting post form data
